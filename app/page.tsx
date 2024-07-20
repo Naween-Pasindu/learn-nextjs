9452ef0ffca2358d860c9fe9744f06c439194b4b
Naween Pasindu <Naween-Pasindu@users.noreply.github.com>
2024-07-20 19:44:56 +0530

    sercer side and client side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello world
+      <Link href="/users" >Users</Link>
+      <ProductCard/>
     </main>
   );
 }
